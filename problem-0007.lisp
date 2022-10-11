59eec01f50e18c26a86632423201d5607a22189f
SoTypicalNoob <so.typical.noob@gmail.com>
2022-10-11 21:10:07 +0200

    Improvments

--- problem-0007.lisp
+++ problem-0007.lisp
@@ -1,8 +1,6 @@
 ;; By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 ;; What is the 10 001st prime number?
 
-(defparameter *prime-list* (list))
-
 (defun prime-p (n)
   (if (member T (loop :for i :from 2 :to (isqrt n)
                       :collect (integerp (/ n i))))
@@ -12,8 +10,8 @@
 ;; n: nth number.
 ;; x: optional. it starts the count from 2 if no other
 ;; number is specified.
-(defun nth-prime (n &optional (x 2) )
+(defun nth-prime (n &optional (x 2) (prime-list (list)))
   (if (prime-p x)
-      (setf *prime-list* (push x *prime-list*)))
-  (cond ((<= n (length *prime-list*) ) *prime-list*)
-        (t (nth-prime n (+ x 1)))))
+      (setf prime-list (push x prime-list)))
+  (cond ((<= n (length prime-list) ) (car  prime-list))
+        (t (nth-prime n (+ x 1) prime-list))))
