e1c260abed63bafcf4488b39fdebcaa919d056c3
SoTypicalNoob <so.typical.noob@gmail.com>
2022-11-09 21:42:24 +0100

    further thinking/work on problem 50

--- problem-0050.lisp
+++ problem-0050.lisp
@@ -8,5 +8,34 @@
 
 (defun consecutive-primes (n &optional (x 2) (prime-list (list)))
   (let ((sum-prime (apply '+ prime-list)))
-    (cond ((and (<= n sum-prime) (prime-p sum-prime)) sum-prime)
+    (if (prime-p x)
+        (progn
+          (setf prime-list (push x prime-list))
+          (test))
+        (consecutive-primes n (+ x 1) prime-list))
+    (cond ((and (<= sum-prime n) (prime-p sum-prime))
+           (format t "~A~%" sum-prime))
           (t (consecutive-primes n (+ x 1) prime-list)))))
+
+(defun consecutive-primes (n &optional (x 2) (prime-list (list)) (sum-prime 0))
+  (if (<= sum-prime n)
+      (if (prime-p x)
+          (progn
+            (setf prime-list (push x prime-list))
+            (setf sum-prime (+ x sum-prime))
+            (consecutive-primes n (+ x 1) prime-list sum-prime))
+          (consecutive-primes n (+ x 1) prime-list sum-prime))
+      (reverse (cdr prime-list))))
+
+(defun find-sum-prime (consecutive-primes)
+  (let ((sum-prime (apply '+ consecutive-primes)))
+    (if (prime-p sum-prime)
+        sum-prime
+        (find-sum-prime (cdr consecutive-primes)))))
+
+(defun find-sum-prime-reverse (consecutive-primes)
+  (let* ((reverse-primes (reverse consecutive-primes))
+         (sum-prime (apply '+ reverse-primes)))
+    (if (prime-p sum-prime)
+        sum-prime
+        (find-sum-prime (reverse (cdr reverse-primes))))))
