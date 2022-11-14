b1ecf6be299fa7d1f2befcf942437aa2d3c5cdd8
SoTypicalNoob <so.typical.noob@gmail.com>
2022-11-14 21:09:59 +0100

    Summation of Primes

---
+++ problem-0010.lisp
@@ -0,0 +1,11 @@
+;; The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+;; Find the sum of all the primes below two million.
+
+(load "/home/n00b/projects/project_euler/my-math-funcs.lisp")
+
+(defun sum-of-primes (n &optional (x 2) (sum-prime 0))
+  (if (<= x n)
+      (if (prime-p x)
+          (sum-of-primes n (+ x 1) (+ sum-prime x))
+          (sum-of-primes n (+ x 1) sum-prime))
+      sum-prime))
